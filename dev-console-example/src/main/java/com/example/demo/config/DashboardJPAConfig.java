2be5b84f2b9ac1e4df620cd5aa276ff74946726c
Mariam Cueto <Mariam.Cueto@chenmed.com>
2022-12-05 11:24:52 -0500

    Continued DataSource Config

---
+++ dev-console-example/src/main/java/com/example/demo/config/DashboardJPAConfig.java
@@ -0,0 +1,42 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        basePackages = "com.example.demo.repositories",
+        entityManagerFactoryRef = "dashboardEntityManagerFactory",
+        transactionManagerRef = "dashboardTransactionManager"
+)
+
+public class DashboardJPAConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean dashboardEntityManagerFactory(
+            @Qualifier("dashboardDataSource") DataSource dataSource,
+            EntityManagerFactoryBuilder builder) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.example.demo.repositories")
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager dashboardTransactionManager(
+            @Qualifier("dashboardEntityManagerFactory") LocalContainerEntityManagerFactoryBean dashboardEntityManagerFactory) {
+        return new JpaTransactionManager(Objects.requireNonNull(dashboardEntityManagerFactory.getObject()));
+    }
+
+}
