da0c4384b2d190b3f42ae7e06a6399dc106b1258
Harrison Holsinger <harrisonholsinger@chenmed.com>
2022-12-05 10:25:50 -0500

    Changed file and application name

--- dev-console-example/src/main/java/com/example/demo/DevConsoleExampleApplication.java
+++ dev-console-example/src/main/java/com/example/demo/DevConsoleExampleApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevConsolePersonApplication {
+public class DevConsoleExampleApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(DevConsolePersonApplication.class, args);
+		SpringApplication.run(DevConsoleExampleApplication.class, args);
 	}
 
 }
